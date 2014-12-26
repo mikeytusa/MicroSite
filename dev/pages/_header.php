79cd06244ec3725ea57d80b7c7040a871e1a2b15
Mike Thompson <mike@coldplatter.com>
2014-12-26 12:48:09 -0700

    Added custom Chrome tab color meta tag

--- dev/pages/_header.php
+++ dev/pages/_header.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?= $title ?></title>
-    <meta name="description" content="<?= $desc ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Let's load up some stylesheets -->
-    <link rel="stylesheet" href="/assets/css/common/main.css">
-    <!-- Apple icons -->
-    <link rel="apple-touch-icon" href="/assets/img/apple/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/assets/img/apple/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/assets/img/apple/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/assets/img/apple/apple-touch-icon-144x144.png">
-    
-    <!-- Header includes for this page -->
-    <!-- The text in the parethentheses is the indent should precede each line, for prettier source -->
-    <?php output_header_includes('    '); ?>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title><?= $title ?></title>
+<meta name="description" content="<?= $desc ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Let's load up some stylesheets -->
+<link rel="stylesheet" href="/assets/css/common/main.css">
+<!-- Apple icons -->
+<link rel="apple-touch-icon" href="/assets/img/apple/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/assets/img/apple/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/assets/img/apple/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/assets/img/apple/apple-touch-icon-144x144.png">
+<!-- Change this color to change the color of Chrome's tab on mobile to match your website -->
+<meta name="theme-color" content="#4F71B0">
+<!-- Header includes for this page -->
+<!-- The text in the parethentheses is the indent that should precede each line (if this head section was tabbed), for prettier source -->
+<?php output_header_includes(''); ?>
 </head>
  
 <body>
